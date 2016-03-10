a45546562edc68b029762e7dc98de96a6d67f7a6
Piyush Agrawal <poush12@gmail.com>
2016-03-10 23:13:40 +0530

    Email Template

--- app/Listeners/OrderPlacedListener.php
+++ app/Listeners/OrderPlacedListener.php
@@ -32,7 +32,7 @@ class OrderPlacedListener
         \Slack::send(
                 "=========\n Hey team !! \n We have new Order \n=======\n $order->customer_name\n $order->customer_number*\n $order->customer_email\n Final Price: Rs. ". ($order->total-$order->discount)." ```$raw_products```"
         );
-        \Mail::send('emails.welcome', ['data' => 'data'], function ($message) use($order) {
+        \Mail::send('emails.orderPlaced', ['data' => 'data'], function ($message) use($order) {
         $message->sender('do-not-reply@KhairdTo.com',' Your order at KhairdTo');
 
         $message->to($order->customer_email);
