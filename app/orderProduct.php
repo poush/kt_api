ce01136cc7fa625005930d08ffa9c50b62d8fdc8
Piyush Agrawal <poush12@gmail.com>
2016-03-23 17:37:39 +0530

    Order Placement fix to calculate final price

--- app/orderProduct.php
+++ app/orderProduct.php
@@ -8,5 +8,5 @@ class orderProduct extends Model
 {
  	protected $table = 'order_products';
 
- 	protected $fillable = ['product_code','order_id','quantity','product_id','price','discount','product_name'];
+ 	protected $fillable = ['product_code','order_id','quantity','product_id','price','discount','product_name','final'];
 }
