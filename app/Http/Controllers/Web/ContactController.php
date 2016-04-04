e104046e81c542d8efd5f1162afcb4e72ac93eb1
Piyush Agrawal <poush12@gmail.com>
2016-04-04 20:57:39 +0530

    contact fix

--- app/Http/Controllers/Web/ContactController.php
+++ app/Http/Controllers/Web/ContactController.php
@@ -24,7 +24,6 @@ class ContactController extends Controller
 
         \Mail::raw($request->message, function($m) use($request){
             $m->from('do-no-reply@khareedto.com', 'KhareedTo');
-            $m->replyTo($request->email);
             $m->subject('New Contact Request at KhareedTo');
             $m->to('alltimepresent@gmail.com');
 
