efcc653fbb2ecc6078b4bdbedc18fbffb29bc891
Piyush Agrawal <poush12@gmail.com>
2016-04-04 19:37:16 +0530

    Contact api fix

--- app/Http/Controllers/Web/ContactController.php
+++ app/Http/Controllers/Web/ContactController.php
@@ -23,6 +23,7 @@ class ContactController extends Controller
             throw new ValidationHttpException($validator->errors());
 
         Mail::send('emails.contact', ['data' => $request->input('message')], function($m) use($request){
+            $m->from('do-no-reply@kharidto.com');
             $m->replyTo($request->email);
             $m->to('care@khareedto.com', 'New contact request');
 
